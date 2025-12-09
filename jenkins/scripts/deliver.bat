79632057f79492b71ca403e0f3d554089171732a
Ignacio Nicolas Basilio Buracco <71569917+NachoBasilio@users.noreply.github.com>
2025-12-09 19:19:31 -0300

    Cambie el SH a un BAT

---
+++ jenkins/scripts/deliver.bat
@@ -0,0 +1,7 @@
+@echo off
+setlocal enabledelayedexpansion
+
+echo Running the Java application...
+java -jar target\my-app.jar
+
+endlocal
--- jenkins/scripts/deliver.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-#!/bin/bash
-set -e
-
-echo "Running the Java application..."
-java -jar target/my-app.jar
